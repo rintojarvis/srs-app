62bec79431603fb72e1c919a460a9445a059f144
rintojarvis <rinto0210@gmail.com>
2026-05-17 00:45:24 +0900

    auto: 2026-05-17 00:45

---
+++ supabase/config.js
@@ -0,0 +1,6 @@
+// Supabase クライアント設定
+// anon (publishable) key は RLS で保護されるためブラウザに露出して OK
+// service_role キーは絶対に含めないこと
+
+export const SUPABASE_URL = 'https://dxtparataktbxsxyzseq.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_tS_e5Awv7ZHuyanN2JoE1A_WWpmO5J2';
